66d1e035847c8292b64458c32e83070111a292a6
jason.meckley <jason.meckley@validusresearch.com>
2019-06-24 10:59:09 -0400

    fix character compile error

--- src/app/characters/characters-routing.module.ts
+++ src/app/characters/characters-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CharactersShellComponent } from './characters-shell/characters-shell.component';
 import { CharacterShellComponent } from './character-shell/character-shell.component';
+import { CharacterExistsGuard } from './character-exists.guard';
 
 const routes: Routes = [
   {
